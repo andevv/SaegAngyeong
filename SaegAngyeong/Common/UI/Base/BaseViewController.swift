f46f5859e930f1e25270455918760d0536a26c99
andev <andh1006@naver.com>
2025-12-14 17:08:23 +0900

    feat: BaseVC, BaseVM 구현

---
+++ SaegAngyeong/Common/UI/Base/BaseViewController.swift
@@ -0,0 +1,40 @@
+//
+//  BaseViewController.swift
+//  SaegAngyeong
+//
+//  Created by andev on 12/14/25.
+//
+
+import UIKit
+import Combine
+
+class BaseViewController<ViewModel: ViewModelType>: UIViewController {
+
+    // MARK: - Properties
+    let viewModel: ViewModel
+    var cancellables = Set<AnyCancellable>()
+
+    // MARK: - Initializer
+    init(viewModel: ViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) is not supported")
+    }
+
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureUI()
+        configureLayout()
+        bindViewModel()
+    }
+
+    // MARK: - Template Methods (Override Points)
+    func configureUI() {}
+    func configureLayout() {}
+    func bindViewModel() {}
+}
---
+++ SaegAngyeong/Common/UI/Base/BaseViewModel.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewModel.swift
+//  SaegAngyeong
+//
+//  Created by andev on 12/14/25.
+//
+
+import Combine
+
+/// 모든 ViewModel이 채택하는 기본 프로토콜
+protocol ViewModelType {
+    associatedtype Input
+    associatedtype Output
+
+    func transform(input: Input) -> Output
+}
+
+/// 공통 기능을 제공하는 베이스 ViewModel (선택적 상속)
+class BaseViewModel {
+
+    // MARK: - Common Outputs
+    let isLoading = PassthroughSubject<Bool, Never>()
+    let error = PassthroughSubject<Error, Never>()
+
+    // MARK: - Combine
+    var cancellables = Set<AnyCancellable>()
+
+    init() {}
+}
