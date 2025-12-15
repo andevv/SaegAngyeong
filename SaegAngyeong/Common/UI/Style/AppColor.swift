37df91110062b88d2e12e62dfc0abe60b2d8dbea
andev <andh1006@naver.com>
2025-12-15 18:45:34 +0900

    feat: AppColor 코드 추가

---
+++ SaegAngyeong/Common/UI/Style/AppColor.swift
@@ -0,0 +1,26 @@
+//
+//  AppColor.swift
+//  SaegAngyeong
+//
+//  Created by andev on 12/15/25.
+//
+
+import UIKit
+
+enum AppColor {
+
+    // Gray Scale
+    static let gray0 = UIColor(named: "Gray_0")!
+    static let gray15 = UIColor(named: "Gray_15")!
+    static let gray30 = UIColor(named: "Gray_30")!
+    static let gray45 = UIColor(named: "Gray_45")!
+    static let gray60 = UIColor(named: "Gray_60")!
+    static let gray75 = UIColor(named: "Gray_75")!
+    static let gray90 = UIColor(named: "Gray_90")!
+    static let gray100 = UIColor(named: "Gray_100")!
+
+    // Turquoise
+    static let blackTurquoise = UIColor(named: "BlackTurquoise")!
+    static let brightTurquoise = UIColor(named: "BrightTurquoise")!
+    static let deepTurquoise = UIColor(named: "DeepTurquoise")!
+}
