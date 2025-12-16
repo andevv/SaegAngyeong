8fc8b76fe6d1112dd3fc9ecbf2e4c0cf45225a0d
andev <andh1006@naver.com>
2025-12-16 21:26:56 +0900

    feat: 탭바 아이콘 색상 변경

--- SaegAngyeong/Features/Main/MainTabBarController.swift
+++ SaegAngyeong/Features/Main/MainTabBarController.swift
@@ -11,12 +11,18 @@ final class MainTabBarController: UITabBarController {
 
     init(dependency: AppDependency) {
         super.init(nibName: nil, bundle: nil)
+        configureAppearance()
         setupTabs(dependency: dependency)
     }
 
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) is not supported")
+    }
+
+    private func configureAppearance() {
+        tabBar.tintColor = .gray15
+        tabBar.unselectedItemTintColor = .gray45
     }
 
     private func setupTabs(dependency: AppDependency) {
@@ -28,10 +34,10 @@ final class MainTabBarController: UITabBarController {
         )
         let homeVC = HomeViewController(viewModel: homeVM)
 
-        let dummy1 = DummyViewController(titleText: "", named: "Feed_Fill", tag: 1, color: .systemGray6)
-        let dummy2 = DummyViewController(titleText: "", named: "Filter_Fill", tag: 2, color: .systemGray5)
-        let dummy3 = DummyViewController(titleText: "", named: "Search_Fill", tag: 3, color: .systemGray4)
-        let dummy4 = DummyViewController(titleText: "", named: "Profile_Fill", tag: 4, color: .systemGray3)
+        let dummy1 = DummyViewController(titleText: "", named: "Feed_Empty", tag: 1, color: .systemGray6)
+        let dummy2 = DummyViewController(titleText: "", named: "Filter_Empty", tag: 2, color: .systemGray5)
+        let dummy3 = DummyViewController(titleText: "", named: "Search_Empty", tag: 3, color: .systemGray4)
+        let dummy4 = DummyViewController(titleText: "", named: "Profile_Empty", tag: 4, color: .systemGray3)
 
         viewControllers = [
             homeVC,
